c16fcb65728366aac5359cc1d101764dc0e043bd
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-03-21 00:13:14 +0100

    Begin implementation of Model

---
+++ mongo_thingy/model.py
@@ -0,0 +1,19 @@
+from thingy import classproperty, DatabaseThingy
+
+
+class Model(DatabaseThingy):
+    _collection = None
+
+    @classproperty
+    def collection(cls):
+        return cls._collection or cls._table
+
+    @classmethod
+    def _get_database_from_table(cls, collection):
+        return collection.database
+
+    @classmethod
+    def _get_table_from_database(cls, database):
+        if cls.table_name:
+            return getattr(database, cls.table_name)
+        raise AttributeError("Undefined table.")
