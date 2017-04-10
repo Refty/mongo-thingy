49c22b59e6bc0adf52adff158e5179570ce9f8c7
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-10 12:09:26 +0200

    Rename Model to Thingy

--- mongo_thingy/__init__.py
+++ mongo_thingy/__init__.py
@@ -1,9 +1,33 @@
 from pymongo import MongoClient
+from thingy import classproperty, DatabaseThingy
 
-from .model import Model
+
+class Thingy(DatabaseThingy):
+    client = None
+    _collection = None
+
+    @classproperty
+    def collection(cls):
+        return cls._collection or cls.table
+
+    @classproperty
+    def collection_name(cls):
+        return cls.collection.name
+
+    @classproperty
+    def _table(cls):
+        return cls._collection
+
+    @classmethod
+    def _get_database_from_table(cls, collection):
+        return collection.database
+
+    @classmethod
+    def _get_table_from_database(cls, database):
+        return database[cls.table_name]
 
 
 def connect(*args, **kwargs):
     client = MongoClient(*args, **kwargs)
-    Model.client = client
+    Thingy.client = client
     return client
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,37 @@
+import mongomock
 from pymongo import MongoClient
 
-from mongo_thingy import connect, Model
+from mongo_thingy import connect, Thingy
+
+
+def test_thingy_collection():
+    collection = mongomock.MongoClient().database.collection
+
+    class Foo(Thingy):
+        _collection = collection
+
+    assert Foo.collection == collection
+
+
+def test_thingy_get_database_from_table():
+    collection = mongomock.MongoClient().database.collection
+
+    class Foo(Thingy):
+        _collection = collection
+
+    assert isinstance(Foo.database, mongomock.Database)
+
+
+def test_thingy_get_table_from_database():
+    database = mongomock.MongoClient().database
+
+    class Foo(Thingy):
+        _database = database
+
+    assert Foo.collection_name == "foo"
 
 
 def test_connect():
-    assert Model.client is None
+    assert Thingy.client is None
     connect()
-    assert isinstance(Model.client, MongoClient)
+    assert isinstance(Thingy.client, MongoClient)
