8b4cee3efd865e052bddfb02e3478203dc8ce42f
Guillaume Gelin <ramnes@1000mercis.com>
2017-04-20 18:39:50 +0200

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="Mongo-Thingy",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/numberly/mongo-thingy",
     license="MIT",
     author="Guillaume Gelin",
