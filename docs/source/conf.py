5e05b41f09096d5fef7ce23abc65be86151e9fe5
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2018-03-29 15:47:51 +0000

    Setup documentation build

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- General configuration ------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = 'Mongo-Thingy'
+copyright = 'numberly'
+author = 'numberly'
+
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
