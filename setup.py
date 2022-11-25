e2be3006b3a105e36ff20b47a1f1f7cdc2dd5319
ramnes <contact@ramnes.eu>
2022-11-25 14:01:32 +0100

    Release 0.15.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="Mongo-Thingy",
-    version="0.14.0",
+    version="0.15.0",
     url="https://github.com/Refty/mongo-thingy",
     license="MIT",
     author="Guillaume Gelin",
