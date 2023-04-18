14313c20ab1ae76bc4b7916cfd83ae24534232e6
ramnes <contact@ramnes.eu>
2023-04-18 14:13:39 +0200

    re.sub can be expensive when ran thousands of time

--- mongo_thingy/camelcase.py
+++ mongo_thingy/camelcase.py
@@ -7,13 +7,13 @@ UNCAMELIZE_RE = re.compile(r"(?<!^)(?<![A-Z])[A-Z]")
 
 
 def camelize(string):
-    if string.startswith("__"):
+    if string.startswith("__") or "_" not in string:
         return string
     return re.sub(CAMELIZE_RE, lambda m: m.group(1).upper(), string)
 
 
 def uncamelize(string):
-    if string.startswith("__"):
+    if string.startswith("__") or "_" in string:
         return string
     return re.sub(UNCAMELIZE_RE, r"_\g<0>", string).lower()
 
